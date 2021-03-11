c99bc3b3236503c415486f572bb15d7b237aa4b9
DESKTOP-0BFMIA5\PC-05 <yongseok9028@gmail.com>
2021-03-11 12:52:11 +0900

    SELECT 쿼리 기본문법

---
+++ 2021_03_11.sql
@@ -0,0 +1,30 @@
+SELECT *
+FROM emp;
+
+select *
+from emp;
+
+-- 데이터 조회 방법
+-- FROM : 데이터 조회할 테이블 명시
+-- SELECT : 조회 하고자 하는 컬럼명(테이블에 있는 컬럼명)
+--          테이블의 모든 컬럼을 조회할 경우 *를 기술
+SELECT empno, ename
+FROM EMP;
+-- 구분자는 ,(콤마)
+-- EMPNO : 직원번호, ENAME : 직원이름, JOB : 담당업무
+-- MGR : 상위 담당자, HIREDATE : 입사일자, SAL : 급여
+-- COMM : 상여금, DEPTNO : 부서번호
+-- 자주 쓰일 테이블, 컬럼명 외우자!!
+
+
+
+
+
+
+
+
+
+
+
+
+
